e478c9135a223441347c935592775faa3a27b4af
Niloy Majumdar <niloymajumdar04@gmail.com>
2024-12-03 04:52:37 +0530

    v2

--- js/script.js
+++ js/script.js
@@ -1,15 +1,55 @@
-/* On-scroll Navbar*/
-window.onscroll = () => {
-let header = document.querySelector('.header');
+  /* Menu Icon Navbar */
+  let menuIcon = document.querySelector('#menu-icon');
+  let navbar = document.querySelector('.navbar');
+  let header = document.querySelector('.header');
 
+  
+  menuIcon.onclick = () =>{
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+    header.classList.toggle('active');
+    
+  };
+
+
+/* Scroll Scetion Active Links*/
+
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a')
+
+window.onscroll = () => {
+    sections.forEach(sec =>{
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height){
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            })
+        };
+    });
+
+/* On-scroll Navbar*/
 header.classList.toggle('sticky', window.scrollY > 100);
+
+/* remove menu icon navbar */
+menuIcon.classList.remove('bx-x');
+navbar.classList.remove('active');
 };
 
-/* swiper */
-var swiper = new Swiper(".mySwiper", {
+
+
+
+/* Swiper */
+
+const swiper = new Swiper(".mySwiper", {
     slidesPerView: 1,
-    spaceBetween: 30,
+    spaceBetween: 50,
     loop: true,
+    grabCursor:true,
     pagination: {
       el: ".swiper-pagination",
       clickable: true,
@@ -19,3 +59,5 @@ var swiper = new Swiper(".mySwiper", {
       prevEl: ".swiper-button-prev",
     },
   });
+
+
