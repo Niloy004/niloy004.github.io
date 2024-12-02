da501fef707d590207d28613c717efd286f1a616
Niloy Majumdar <niloymajumdar04@gmail.com>
2024-12-03 01:06:11 +0530

    v1

--- js/script.js
+++ js/script.js
@@ -1,6 +1,21 @@
-
+/* On-scroll Navbar*/
 window.onscroll = () => {
 let header = document.querySelector('.header');
 
 header.classList.toggle('sticky', window.scrollY > 100);
 };
+
+/* swiper */
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
