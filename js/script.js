d92bfa72d7ab9fd86c35b1dd1d6ec97b3dc5a812
Niloy Majumdar <niloymajumdar04@gmail.com>
2024-12-03 18:42:40 +0530

    v1.0

--- js/script.js
+++ js/script.js
@@ -41,8 +41,6 @@ navbar.classList.remove('active');
 };
 
 
-
-
 /* Swiper */
 
 const swiper = new Swiper(".mySwiper", {
@@ -60,4 +58,22 @@ const swiper = new Swiper(".mySwiper", {
     },
   });
 
+/* Dark Mode */
+let darkModeIcon = document.querySelector('#darkMode-icon');
 
+darkModeIcon.onclick = () => {
+  darkModeIcon.classList.toggle('bx-sun');
+  document.body.classList.toggle('dark-mode');
+}
+
+/* Scroll Reveal */
+ScrollReveal({ 
+  reset: true,
+  distance: '80px',
+  duration: 2000,
+  delay: 200
+});
+
+ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
+ScrollReveal().reveal('.home-img img, .edu-container, .skill-wrapper, .contact form', { origin: 'bottom' });
+ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left' });
